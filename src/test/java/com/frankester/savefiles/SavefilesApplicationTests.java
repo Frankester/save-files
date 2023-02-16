ec33d097f597e7d78ef6ec16f56a0cb446713a9c
Frankester <francotomascallero@gmail.com>
2023-02-16 12:32:20 -0300

    init project structure

---
+++ src/test/java/com/frankester/savefiles/SavefilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankester.savefiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavefilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
